f3ec95d10a2fd1e352a33ec5535aa9a06e2b17c0
lapejoou <129972358+lapejoou@users.noreply.github.com>
2023-04-10 19:32:11 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,2 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello from feature")
-=======
->>>>>>> b23547b242012497282910300f6e6c1d5a27df82
-=======
 print("Hello once again")
->>>>>>> d67b3c03b79dac9d7aff6075d461aab24bdfe673
